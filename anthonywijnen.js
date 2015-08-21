58d93bb54e39a0dfd699abe9bf6fe64921204622
Anthony Wijnen <anthony.wijnen@gmail.com>
2015-08-21 12:41:39 +0200

    first commit

---
+++ anthonywijnen.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+  Template.hello.helpers({
+
+  });
+
+  Template.hello.events({
+
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
